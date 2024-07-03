941c426db1d6db990c0fbbd939d9b9729c6ffe6d
brace <brace@qq.com>
2024-07-03 14:09:08 +0800

    init config

--- src/renderer/src/main.js
+++ src/renderer/src/main.js
@@ -1,4 +1,19 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// 引入 Element UI
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+// 国际化
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
+// Router
+import router from '@/router'
+
+const app = createApp(App)
+
+app.use(router)
+
+app.use(ElementPlus, {
+  locale: zhCn
+})
+app.mount('#app')
--- src/renderer/src/views/Login.vue
+++ src/renderer/src/views/Login.vue
@@ -1,9 +1,13 @@
 <script setup>
+import { ref, reactive, getCurrentInstance, nextTick } from 'vue'
 
+const { proxy } = getCurrentInstance()
 </script>
 
 <template>
-  $END$
+  <div>
+    <el-button type="primary" @click=''> 按钮</el-button>
+  </div>
 </template>
 
 <style scoped lang="less">
